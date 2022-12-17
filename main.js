b7adc52a87c8c3450fa53da9b8c10abf506ab439
Trinh Van Quyen <quyentv@timebird.org>
2022-12-17 16:24:26 +0700

    update code

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('aaaaaa');
+var menuBtn = document.getElementById('menuBtn')
+console.log(menuBtn);
+var menuList = document.getElementById('menu-list')
+var overlay = document.getElementById('overlay-menu')
+var btnClose = document.getElementById('btn-close')
+menuBtn.addEventListener("click", function(){ 
+  console.log(menuList);
+  menuList.classList.add("open");
+  overlay.classList.add("open");
+});
+btnClose.addEventListener("click", function(){ 
+  console.log(menuList);
+  menuList.classList.remove("open");
+  overlay.classList.remove("open");
+});
